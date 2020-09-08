6849d39f1ee01dabe8e2a8c6bd6142539a7d74c8
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-09-07 21:25:34 -0400

    Fixed defender bug (invalid game object in memory -- can't reuse room position object)

--- src/actionPatrol.ts
+++ src/actionPatrol.ts
@@ -10,15 +10,14 @@ export const actionPatrol = (creep: Creep) => {
   const terrain = new Room.Terrain(creep.room.name)
 
   switch (terrain.get(creep.memory.destination.x, creep.memory.destination.y)) {
+    // No action cases
     case 0: // plain (valid destination)
       break
+    // Change destination cases
     case TERRAIN_MASK_WALL:
-    // intentionally don't break
     case TERRAIN_MASK_SWAMP:
-    // intentionally don't break
     default:
       changeDestination(creep)
-      break
   }
 
   if (
@@ -31,8 +30,31 @@ export const actionPatrol = (creep: Creep) => {
     changeDestination(creep)
   } else {
     // Get to moving
-    creep.moveTo(creep.memory.destination, {
-      visualizePathStyle: { stroke: "#ffaa00" },
-    })
+    const moveResult = creep.moveTo(
+      creep.memory.destination.x,
+      creep.memory.destination.y,
+      {
+        visualizePathStyle: { stroke: "#ffaa00" },
+        reusePath: 5, // Disable path reuse; TODO This uses a lot of CPU
+      }
+    )
+    switch (moveResult) {
+      // Do nothing cases
+      case OK: // The operation has been scheduled successfully.
+      case ERR_TIRED: // The fatigue indicator of the creep is non-zero.
+        break // Do nothing
+      // Change source case (There are probably creeps in the way)
+      case ERR_NO_PATH: // No path to the target could be found.
+        changeDestination(creep)
+        break
+      // Unhandled cases
+      case ERR_NOT_OWNER: // You are not the owner of this creep.
+      case ERR_BUSY: // The power creep is not spawned in the world.
+      case ERR_INVALID_TARGET: // The target provided is invalid.
+      default:
+        console.log(
+          `${creep.name} had an unexpected error in move routine: ${moveResult}`
+        )
+    }
   }
 }
