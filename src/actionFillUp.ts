efcc5a40569fbe6e7b91d0fe61acedf67905356b
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-09-08 20:14:26 -0400

    Added back ability to withdraw from containers to fill up logic

--- src/actionFillUp.ts
+++ src/actionFillUp.ts
@@ -1,78 +1,44 @@
 export const actionFillUp = (creep: Creep) => {
   creep.say("🚶 FILL UP")
-  const targetFillUpSite = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-    // var targets = creep.room.find(FIND_MY_STRUCTURES, {
-    // var targets = Game.spawns["Spawn1"].room.find(FIND_MY_STRUCTURES, {
+  const targetFillUpSite = creep.pos.findClosestByPath(FIND_STRUCTURES, {
     filter: (structure) => {
       return (
-        structure.structureType === STRUCTURE_SPAWN &&
-        structure.store.getUsedCapacity(RESOURCE_ENERGY) >= 50
-        /*        (structure.structureType == STRUCTURE_EXTENSION ||
-          structure.structureType == STRUCTURE_SPAWN ||
-          structure.structureType == STRUCTURE_TOWER ||
-          structure.structureType == STRUCTURE_CONTAINER ||
-          structure.structureType == STRUCTURE_STORAGE) &&
-        structure.store.getUsedCapacity(RESOURCE_ENERGY) >= 50*/
+        structure.structureType === STRUCTURE_CONTAINER &&
+        structure.store.getUsedCapacity(RESOURCE_ENERGY) >= 1000
       )
     },
   })
 
-  const allDroppedResources = creep.room.find(FIND_DROPPED_RESOURCES)
-  const sortedDroppedResources = _.sortBy(
-    allDroppedResources,
-    (resource) =>
-      // 0 - resource.amount / creep.pos.findPathTo(resource.pos).length
-      0 - resource.amount ** 2 / creep.pos.getRangeTo(resource.pos)
-    // zero minus is Ascending sort
-  )
-  for (const resource of sortedDroppedResources) {
-    console.log(resource.amount ** 2 / creep.pos.getRangeTo(resource.pos))
-  }
-  //
-  console.log("ddfdfdfdfdf")
-
-  const droppedResourceTarget = sortedDroppedResources[0]
-
-  // Maybe there are some dropped resources we can go grab
-
-  /*
-  let droppedResourceTarget = creep.pos.findClosestByPath(
-    FIND_DROPPED_RESOURCES,
-    {
-      filter(resource) {
-        return resource.amount >= 300
-      },
-    }
-  )
-
-  if (droppedResourceTarget === null) {
-    droppedResourceTarget = creep.pos.findClosestByPath(
-      FIND_DROPPED_RESOURCES,
-      {
-        filter(resource) {
-          return resource.amount >= 100
-        },
-      }
-    )
-  }
-
-  if (droppedResourceTarget === null) {
-    droppedResourceTarget = creep.pos.findClosestByPath(
-      FIND_DROPPED_RESOURCES,
-      {
-        filter(resource) {
-          return resource.amount >= 0
-        },
-      }
-    )
-  }*/
-
-  if (droppedResourceTarget != null) {
-    creep.say("🔄 PICK UP")
-    if (creep.pickup(droppedResourceTarget) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(droppedResourceTarget, {
+  if (targetFillUpSite) {
+    creep.say("🚶 WITHDRAW")
+    if (
+      creep.withdraw(targetFillUpSite, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE
+    ) {
+      creep.moveTo(targetFillUpSite, {
         visualizePathStyle: { stroke: "#ffaa00" },
       })
+    }
+  } else {
+    const allDroppedResources = creep.room.find(FIND_DROPPED_RESOURCES)
+    const sortedDroppedResources = _.sortBy(
+      allDroppedResources,
+      (resource) =>
+        // 0 - resource.amount / creep.pos.findPathTo(resource.pos).length
+        0 - resource.amount ** 2 / creep.pos.getRangeTo(resource.pos)
+      // zero minus is Ascending sort
+    )
+
+    const droppedResourceTarget = sortedDroppedResources[0]
+
+    // Maybe there are some dropped resources we can go grab
+
+    if (droppedResourceTarget != null) {
+      creep.say("🔄 PICK UP")
+      if (creep.pickup(droppedResourceTarget) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(droppedResourceTarget, {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        })
+      }
     }
   }
 }
