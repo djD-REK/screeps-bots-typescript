3d1440ec3e85ce014a108e20f0cd7ca05fcb6122
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-05 19:28:52 -0500

    Make eye creeps stop exploring if they're the only creep in the room

--- src/roleEye.ts
+++ src/roleEye.ts
@@ -9,34 +9,40 @@ import {
 
 export const roleEye = {
   run(creep: Creep) {
-    if (
-      !creep.memory.destination ||
-      creep.room.name !== creep.memory.destination.roomName
-    ) {
-      // We have no destination (initial state) OR
-      // We've arrived in another room
-      // UNLESS we have vision of all the adjacent rooms
-      chooseDestination(creep)
-      // TODO: Add an if there's an enemy in this room I need to go home
-    }
-    const accessibleRoomNamesWithoutVision = getAccessibleRoomNamesWithoutVision(
-      creep.room
-    )
-    const destinationRoomName = getRoomNameBasedOnExitCoordinates(
-      creep.memory.destination.x,
-      creep.memory.destination.y,
-      new Room(creep.memory.destination.roomName)
-    )
-    if (
-      Game.rooms[destinationRoomName] &&
-      accessibleRoomNamesWithoutVision.length > 0
-    ) {
-      // Wait a second, we have vision of the destination, so change it,
-      // unless we have vision of all the possible destinations right now.
-      chooseDestination(creep)
-    }
+    // Stop exploring if I'm the only creep in the room
+    if (creep.room.find(FIND_MY_CREEPS).length === 1) {
+      creep.memory.state = "OBSERVE"
+      creep.moveTo(25, 25) // move to the center
+    } else {
+      if (
+        !creep.memory.destination ||
+        creep.room.name !== creep.memory.destination.roomName
+      ) {
+        // We have no destination (initial state) OR
+        // We've arrived in another room
+        // UNLESS we have vision of all the adjacent rooms
+        chooseDestination(creep)
+        // TODO: Add an if there's an enemy in this room I need to go home
+      }
+      const accessibleRoomNamesWithoutVision = getAccessibleRoomNamesWithoutVision(
+        creep.room
+      )
+      const destinationRoomName = getRoomNameBasedOnExitCoordinates(
+        creep.memory.destination.x,
+        creep.memory.destination.y,
+        new Room(creep.memory.destination.roomName)
+      )
+      if (
+        Game.rooms[destinationRoomName] &&
+        accessibleRoomNamesWithoutVision.length > 0
+      ) {
+        // Wait a second, we have vision of the destination, so change it,
+        // unless we have vision of all the possible destinations right now.
+        chooseDestination(creep)
+      }
 
-    // By this point, we'll always have a destination assigned.
-    moveToDestination(creep)
+      // By this point, we'll always have a destination assigned.
+      moveToDestination(creep)
+    }
   },
 }
