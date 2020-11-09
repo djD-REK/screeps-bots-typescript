bdfc532a4cb4e7b8f1f2d47b9e5f7eb422f37c21
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-09 15:40:06 -0500

    Big refactor into state machines for eye creeps

--- src/roleEye.ts
+++ src/roleEye.ts
@@ -11,50 +11,86 @@ export const roleEye = {
   run(creep: Creep) {
     // TODO: Add if there's an enemy in this room I need to go home
 
-    // Stop exploring if this Eye creep is the only creep in the room
-    // and don't have any owned structures, which means this creep is
-    // giving us vision of the room right now
-    if (
-      creep.room.find(FIND_MY_CREEPS).length === 1 &&
-      creep.room.find(FIND_MY_STRUCTURES).length === 0
-    ) {
-      creep.memory.state = "OBSERVE"
-      // move to the center
+    if (creep.memory.state === "THINK") {
+      // STATE TRANSITION: THINK --> EXPLORE
+      // WHEN THIS EYE CREEP NEEDS A NEW DESTINATION
+      // Either this creep is brand new (initial state)
+      // OR it has transitioned to a new room (after exploring)
+      // OR it realizes we now have vision of the new room
+      creep.memory.state = "EXPLORE"
+
+      // STATE ACTION: CHOOSE A NEW DESTINATION
+      chooseDestination(creep)
+    }
+    if (creep.memory.state === "OBSERVE") {
+      // STATE TRANSITION: OBSERVE --> THINK
+      // WHEN SOMETHING ELSE IS PROVIDING VISION
+      if (
+        creep.room.find(FIND_MY_CREEPS).length !== 1 ||
+        creep.room.find(FIND_MY_STRUCTURES).length !== 0
+      ) {
+        creep.memory.state = "THINK"
+      }
+      // STATE ACTION: ASSIGN DESTINATION TO THE MIDDLE
       creep.memory.destination.x = 25
       creep.memory.destination.y = 25
-    } else {
+      // STATE ACTION: MOVE TO THE DESTINATION
+      moveToDestination(creep)
+    }
+    if (creep.memory.state === "EXPLORE") {
+      // STATE TRANSITION: EXPLORE --> OBSERVE
+      // WHEN THIS EYE CREEP IS PROVIDING VISION
       if (
-        !creep.memory.destination ||
-        creep.room.name !== creep.memory.destination.roomName
+        creep.room.find(FIND_MY_CREEPS).length === 1 &&
+        creep.room.find(FIND_MY_STRUCTURES).length === 0
       ) {
-        // We have no destination (initial state) OR
-        // We've arrived in another room
-        chooseDestination(creep)
+        // Stop exploring if this Eye creep is the only creep in the room
+        // and don't have any owned structures, which means this creep is
+        // giving us vision of the room right now
+        creep.memory.state = "OBSERVE"
       }
-      // TODO This is possibly the part that doesn't work
+
+      // STATE TRANSITION: EXPLORE --> THINK
+      // WHEN THE CURRENT DESTINATION IS NO LONGER VALID
+      // Such as when it has transited into a new room
+      // OR it realizes we now have vision of the new room
+      if (creep.room.name !== creep.memory.destination.roomName) {
+        // We've arrived in another room
+        creep.memory.state = "THINK"
+      }
+
+      // STATE TRANSITION: EXPLORE --> THINK
+      // WHEN WE HAVE VISION OF THIS EYE CREEP'S DESTINATION
       const destinationRoomName = getRoomNameBasedOnExitCoordinates(
         creep.memory.destination.x,
         creep.memory.destination.y,
         new Room(creep.memory.destination.roomName)
       )
-      // TODO or maybe it's this part
+      console.log(
+        `Creep ${creep.name} has destination room name of ${destinationRoomName}`
+      )
       const accessibleRoomNamesWithoutVision = getAccessibleRoomNamesWithoutVision(
         creep.room
       )
-      /*
+      console.log(
+        `According to creep ${creep.name}, these are the accessibleRoomNamesWithoutVision: ${accessibleRoomNamesWithoutVision}`
+      )
+
       if (
         Game.rooms[destinationRoomName] &&
         accessibleRoomNamesWithoutVision.length > 0
-      ) {*/
-      // TODO Fix this logic about rooms with vision
-      console.log(`${creep.name} ${destinationRoomName}`)
-      if (Game.rooms[destinationRoomName]) {
+      ) {
+        console.log(
+          `${creep.name} says we have vision of its destination room name, so it's going to pick a new destination`
+        )
         // Wait a second, we have vision of the destination, so change it,
         // unless we have vision of all the possible destinations right now.
-        chooseDestination(creep)
+        creep.memory.state = "THINK"
       }
+
+      // STATE ACTION: MOVE TO THE DESTINATION
+      // By this point, we'll always have a destination assigned
+      moveToDestination(creep)
     }
-    // By this point, we'll always have a destination assigned.
-    moveToDestination(creep)
   },
 }
