39a06796db65295c783096fb9c766c6788d1e9fc
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-17 19:59:55 -0500

    Builders should search all rooms for construction sites now

--- src/actionBuild.ts
+++ src/actionBuild.ts
@@ -5,32 +5,30 @@ import {
 
 export const actionBuild = (creep: Creep) => {
   // Build a structure (i.e. a road) if we are close to one
-  const targetConstructionSite = creep.pos.findClosestByPath(
-    FIND_MY_CONSTRUCTION_SITES
+  let targetConstructionSite = creep.pos.findClosestByPath(
+    FIND_CONSTRUCTION_SITES
   )
+  // It's FIND_CONSTRUCTION_SITES and not FIND_MY_CONSTRUCTION_SITES
+  // because roads and containers are not owned structres.
   if (!targetConstructionSite) {
-    const accessibleAdjacentRooms = getRoomsFromRoomNamesIfVision(
-      getAccessibleAdjacentRoomNames(Game.spawns.Spawn1.room)
-    )
-    // Pick a construction site in a surrounding room with vision
-    // Save the destination to memory
-    // TODO: New state for "transitioning" or "patrolling" to a
-    // different room; then pick the closest site when arrived.
-    for (const accessibleAdjacentRoom of accessibleAdjacentRooms) {
-      const sites = Game.rooms[accessibleAdjacentRoom.name].find(
-        FIND_CONSTRUCTION_SITES
-      )
-      if (sites.length) {
-        creep.memory.destination = new RoomPosition(0, 0, creep.room.name)
-        // destination would be the exit
-        // if the current room name doesn't match, we've transited to the destination
-        // at that point, change state to find a construction site
-        // if there are no sites anywhere, set destination for home spawn
-      }
+    const allConstructionSites: ConstructionSite[] = []
+    // There's no construction site in the creep's current room
+    for (const room of Object.values(Game.rooms)) {
+      allConstructionSites.push(...room.find(FIND_CONSTRUCTION_SITES))
     }
+    allConstructionSites.sort((a, b) => {
+      if (a.room && b.room) {
+        return (
+          Game.map.getRoomLinearDistance(creep.room.name, a.room.name) -
+          Game.map.getRoomLinearDistance(creep.room.name, b.room.name)
+        )
+      }
+      return Infinity
+    })
+    targetConstructionSite = allConstructionSites[0]
   }
   if (targetConstructionSite) {
-    // There is a construction site in the creep's current room
+    // There is a construction site somewhere for us to work on
     const buildResult = creep.build(targetConstructionSite)
     switch (buildResult) {
       // Move to the nearest one
