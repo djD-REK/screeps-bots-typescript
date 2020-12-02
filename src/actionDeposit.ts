b592de7d57825c525c0233093dc7801760cdd2db
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-12-02 12:06:21 -0500

    Pathfinding / traffic jam hotfix

--- src/actionDeposit.ts
+++ src/actionDeposit.ts
@@ -39,7 +39,8 @@ export const actionDeposit = (creep: Creep) => {
     ) {
       const moveResult = creep.moveTo(targetDropOffSite, {
         visualizePathStyle: VISUALIZE_PATH_STYLE,
-        ignoreCreeps: Math.random() < 1 / 2 ? false : true,
+        ignoreCreeps: false, //(default)
+        //ignoreCreeps: Math.random() < 1 / 2 ? false : true,
         // don't ignore creeps when depositing (default is false)
         // this behavior makes creeps stick to the roads
         // which we want, since fetchers benefit from roads
