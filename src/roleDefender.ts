0080623df0e4f90144983047f6ec33ad84141f0f
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-12-01 20:28:39 -0500

    Added basic defender that will move to a miner & guard it

--- src/roleDefender.ts
+++ src/roleDefender.ts
@@ -1,4 +1,5 @@
 import { actionPatrol } from "actionPatrol"
+import { moveToDestination } from "helper_functions"
 
 export const roleDefender = {
   run(creep: Creep) {
@@ -9,7 +10,31 @@ export const roleDefender = {
         creep.moveTo(target)
       }
     } else {
-      actionPatrol(creep)
+      if (creep.memory.state === "THINK") {
+        // we need a home base room (a miner to guard)
+        const allMiners = Array.from(Object.values(Game.creeps)).filter(
+          (creep) => creep.memory.role === "Miner"
+        )
+
+        if (allMiners.length > 0) {
+          const myMiner =
+            allMiners[Math.floor(Math.random() * allMiners.length)]
+          creep.memory.destination.x = myMiner.pos.x
+          creep.memory.destination.y = myMiner.pos.y
+          creep.memory.destination.roomName = myMiner.pos.roomName
+          creep.memory.state = "TRANSIT"
+        }
+      }
+      if (creep.memory.state === "TRANSIT") {
+        moveToDestination(creep)
+        if (creep.room.name === creep.memory.destination.roomName) {
+          creep.memory.state = "GUARD"
+        }
+      }
+      if (creep.memory.state === "GUARD") {
+        // we have a home base to guard
+        actionPatrol(creep)
+      }
     }
   },
 }
