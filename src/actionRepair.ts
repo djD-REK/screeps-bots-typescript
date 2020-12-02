6a3d5c27d9e54026cc3e15dbb34d98aca8d8e4a7
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-12-02 15:51:32 -0500

    Refactor look with offset for bounds

--- src/actionRepair.ts
+++ src/actionRepair.ts
@@ -1,14 +1,13 @@
+import { lookForAtAreaWithOffset } from "helper_functions"
+
 export const actionRepair = (creep: Creep) => {
   // Repair the structure around us with the lowest HP
-  // TODO fix out of bounds errors (map is 0-49)
-  const nearbyStructures = creep.room.lookForAtArea(
-    LOOK_STRUCTURES,
-    creep.pos.y - 3,
-    creep.pos.x - 3,
-    creep.pos.y + 3,
-    creep.pos.x + 3,
-    true
-  ) // we can repair in a 3 range around us
+  const REPAIR_OFFSET = 3 // we can repair in a 3 range around us
+  const nearbyStructures = lookForAtAreaWithOffset(
+    creep,
+    REPAIR_OFFSET,
+    LOOK_STRUCTURES
+  )
   if (nearbyStructures.length > 0) {
     nearbyStructures.sort((a, b) => a.structure.hits - b.structure.hits)
     const repairResult = creep.repair(nearbyStructures[0].structure)
---
+++ src/helper_functions/helpersLooking.ts
@@ -0,0 +1,34 @@
+export const lookForAtAreaWithOffset = (
+  creep: Creep,
+  offset: number,
+  type:
+    | "constructionSite"
+    | "creep"
+    | "energy"
+    | "exit"
+    | "flag"
+    | "mineral"
+    | "deposit"
+    | "nuke"
+    | "resource"
+    | "source"
+    | "structure"
+    | "terrain"
+    | "tombstone"
+    | "powerCreep"
+    | "ruin"
+) => {
+  // The map is an x-y coordinate board from 0-49 (0,0 is top-left)
+  let lookTop = creep.pos.y - offset < 0 ? 0 : creep.pos.y - offset
+  let lookLeft = creep.pos.x - offset < 0 ? 0 : creep.pos.x - offset
+  let lookBottom = creep.pos.y + offset > 49 ? 49 : creep.pos.y + offset
+  let lookRight = creep.pos.x + offset > 49 ? 49 : creep.pos.x + offset
+  return creep.room.lookForAtArea(
+    type, // one of the LOOK_ constants
+    lookTop,
+    lookLeft,
+    lookBottom,
+    lookRight,
+    true // asArray
+  )
+}
